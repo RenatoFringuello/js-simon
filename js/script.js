28ff4e980827b9433238f3bed85a2ae0ef86ba66
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-02 17:28:08 +0100

    add corrections

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@ let count = 0;
 const btn = document.getElementById('btn');
 btn.innerHTML = `#(${count})`;
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function handler(){
     if(count < 10){
         count ++;
     }
     else{
+        console.log('set');
         setTimeout(() => {
             count = 0;
             this.innerHTML = `#(${count})`;
+            btn.addEventListener('click', handler);
         },10000);
+        btn.removeEventListener('click', handler);
     }
     this.innerHTML = `#(${count})`;
 });
